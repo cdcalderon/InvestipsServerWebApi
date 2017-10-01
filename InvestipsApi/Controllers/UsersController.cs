670dc7e193a3be30b91a155ef585bb954094f2ee
Carlos Calderon <cdcalderon@gmail.com>
2017-09-30 22:23:44 -0500

    Add Users Controller

---
+++ InvestipsApi/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Investips.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvestipsApi.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UsersController(IMapper mapper, IUnitOfWork uow)
+        {
+            _unitOfWork = uow;
+            _mapper = mapper;
+        }
+    }
+}
