cc69147145eba617b21931f4f28e4a5cf0ca6f3a
carlos calderon <cdcalderon@gmail.com>
2017-10-16 23:03:46 -0500

    Add AddUserToPortfolio migration

--- Investips.Persistence/Migrations/20171014054132_AddUserToPortfolio.Designer.cs
+++ Investips.Persistence/Migrations/20171014054132_AddUserToPortfolio.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Investips.Persistence.Migrations
 {
     [DbContext(typeof(InvestipsDbContext))]
-    partial class InvestipsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171014054132_AddUserToPortfolio")]
+    partial class AddUserToPortfolio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
